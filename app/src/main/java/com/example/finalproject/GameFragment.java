fb4cdccfad1adb37e8e5b9cdbf62a39e125f355f
katherineanthony <katherinesanthony@gmail.com>
2019-12-13 13:37:55 -0800

    katherine:) -beginning game

--- app/src/main/java/com/example/finalproject/GameFragment.java
+++ app/src/main/java/com/example/finalproject/GameFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,12 +12,13 @@ import androidx.fragment.app.Fragment;
 
 public class GameFragment extends Fragment {
 
-
-
+    private ImageView snapeFace;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+
 
         return container;
     }
